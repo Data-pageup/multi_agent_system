402f83af3dbb7c6e97d59f9331f5d2c125af3366
Amirtha Ganesh <amirthaganeshramesh@gmail.com>
2026-08-18 21:22:01 +0530

    reader & search

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -6,3 +6,22 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 from tools import websearch , scrape_url 
+
+
+load_dotenv()
+
+
+llm = ChatGroq(model = "openai/gpt-oss-20b", temperature= 0 )
+
+
+# agent 1
+
+def  build_search_agent():
+    return create_agent(model =llm, tools = [websearch])
+
+
+# agent 2 
+
+def build_reader_agent():
+    return create_agent(model = llm, tools = [scrape_url])
+
